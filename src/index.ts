9a97359b05c0570ef1616e9996aa50278cb923d9
Alexandre <amoutonbrady@gmail.com>
2020-01-25 18:10:31 +0100

    :bug: Take into account the fact that the Children argument can be an infinity of params

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,34 @@
 import { DOMTags } from './types';
 
-export const baracuda = <Props, Childrens, VDom>(
-	h: (tag: string, props: Props, childrens: Childrens) => VDom,
+export const baracuda = <Props, Children, VDom>(
+	h: (tag: string, props: Props, ...childrens: Children[]) => VDom,
 ) => {
 	// Object validation inspired from: https://github.com/lukeed/klona/blob/master/src/index.js
-	const isObject = (param: Props | Childrens): boolean =>
+	const isObject = (param: Props | Children): boolean =>
 		typeof param === 'object' &&
 		Object.prototype.toString.call(param) === '[object Object]' &&
 		!('props' in param);
 
 	type Tags = {
 		[key in DOMTags]: (
-			props: Props | Childrens,
-			childrens?: Childrens,
+			props: Props | Children,
+			...childrens: Children[]
 		) => VDom;
 	};
 
 	return new Proxy({} as Tags, {
 		get(_, tag: string) {
-			function makeFunction(childrens: Childrens): VDom;
-			function makeFunction(props: Props, childrens: Childrens): VDom;
-			function makeFunction(
-				props: Props | Childrens,
-				childrens?: Childrens,
-			): VDom {
+			function makeFunction(...childrens: Children[]): VDom;
+			function makeFunction(props: Props, ...childrens: Children[]): VDom;
+			function makeFunction(...args: [Props | Children, Children]): VDom {
+				const [props, ...childrens]: [
+					Props | Children,
+					Children,
+				] = args;
+
 				return isObject(props)
-					? h(tag, props as Props, childrens)
-					: h(tag, {} as Props, props as Childrens);
+					? h(tag, props as Props, ...(childrens as Children[]))
+					: h(tag, {} as Props, ...(args as Children[]));
 			}
 
 			return makeFunction;
